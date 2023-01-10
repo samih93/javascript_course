ddc1e6fecb8f1dd1a5e2c7326e5c52f162d23459
samih damaj <samih_damaj@hotmail.com>
2023-01-10 11:42:21 +0200

    asd

--- browser object model 1/1/1.js
+++ browser object model 1/1/1.js
@@ -24,20 +24,12 @@
 
 let range = prompt('“Print Number From – To”');
 
-let nbs = range!==null ? range.split('-'):null;
-let nb1 = nbs !=null? nbs[0]:0;
-let nb2 = nbs !=null? nbs[1]:0;
-if((nb1!==0 || nb2!==0) &&  nb1>nb2)
-{
-    for(let i =nb2 ;i<=nb1;i++)
-    {
-        console.log(i);
-    }
-}
-else
-{
-    for(let i =nb1 ;i<=nb2;i++)
-    {
-        console.log(i);
-    }
+let nbs = range !== null ? range.split('-') : null;
+let nb1 = nbs != null ? nbs[0] : 0;
+let nb2 = nbs != null ? nbs[1] : 0;
+
+var max = nb1 > nb2 ? nb1 : nb2;
+var min = nb1 < nb2 ? nb1 : nb2
+for (let i = min; i <= max; i++) {
+    document.write(`<div>${i}</div>`);
 }
